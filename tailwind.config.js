2b40169bd2cc1173456f6d4f271c5e45357b8568
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-02-15 22:32:56 -0500

    Complete layout and media queries finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/index.html"],
   theme: {
-    extend: {},
+    fontFamily: { 
+      Montserrat: ["Montserrat", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        'primary':"#CC2D4A",
+        'secondary':"#8FA206",
+        'terciary':"#61AEC9"
+      },
+      backgroundImage: {
+        'sanFrancisco': "url('./images/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../images/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../images/yosemite.jpg')",
+			  'LA': "url('../images/LA.jpg')",
+			  'seattle': "url('../images/seattle.jpg')",
+			  'new_york': "url('../images/new_york.jpg')",
+			  'norway': "url('../images/norway.jpg')",
+			  'sydney': "url('../images/sydney.jpg')",
+			  'miami': "url('../images/miami.jpg')",
+			  'switzerland': "url('../images/switzerland.jpg')",
+			  'bali': "url('../images/bali.jpg')",
+			  'chicago': "url('../images/chicago.jpg')",
+			  'europe': "url('../images/europe.jpg')",
+			  'iceland': "url('../images/iceland.jpg')"
+      }
+    },
   },
   plugins: [],
 }
